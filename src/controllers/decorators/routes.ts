00b398a0dadd1bda1055a98698f23c9b112e727f
tomppk <tomi.kivioja@gmail.com>
2021-04-07 11:42:37 +0300

    refactor @get to routeBinder

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,8 +1,9 @@
 // Enable using metadata. Gives access to global Reflect object.
 import 'reflect-metadata';
 
-// Factory decorator that takes url path as argument
-// and returns decorator.
+// Factory decorator that takes url path as argument and returns decorator
+// This factory decorator is wrapped inside another function that takes in the
+// http method (get, post etc.) as argument and passes it down to decorator
 // First argument target prototype of class
 // Second key name of class property, method or accessor
 // Third descriptor of property (eg. writable etc.)
@@ -11,8 +12,22 @@ import 'reflect-metadata';
 // second the value of the metadata key
 // third the class prototype where it is added
 // fourth the key or name of property in that class where metadata is added
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
+// Add two metadata properties 'path' and the http method to use
+
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    };
   };
 }
+
+// Create and export all our different HTTP methods
+// When we call routeBinder with 'get' it is going to return a factory decorator
+// The factory decorator has a reference back to method argument
+export const get = routeBinder('get');
+export const put = routeBinder('put');
+export const post = routeBinder('post');
+export const del = routeBinder('delete');
+export const patch = routeBinder('patch');
